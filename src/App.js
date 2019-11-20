1913fcd0e43e6310995b6abc7607d65fe83833c5
Rawit Panjaroen <check.rawit@gmail.com>
2019-11-20 18:40:03 +0700

    feat: style Webcam & preview image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import React, { useState, useRef } from "react";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
+import React from "react";
+import { CssBaseline, Paper, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { Paper, InputAdornment, IconButton } from "@material-ui/core";
-import InsertDriveFileIcon from "@material-ui/icons/InsertDriveFile";
 
-import CameraIcon from "@material-ui/icons/Camera";
-import Webcam from "react-webcam";
+import ImageUploadForm from "./components/imageUploadForm";
 
 const useStyles = makeStyles(theme => ({
   layout: {
@@ -18,172 +11,14 @@ const useStyles = makeStyles(theme => ({
       width: 600,
       marginLeft: "auto",
       marginRight: "auto"
-    }
+    },
+    paddingTop: theme.spacing(6),
+    paddingBottom: theme.spacing(1)
   },
   paper: {
-    marginTop: theme.spacing(6),
-    marginBottom: theme.spacing(1),
     padding: theme.spacing(3)
-  },
-  inputFile: {
-    margin: `${theme.spacing(0.5)}px 0`
-  },
-  button: {
-    marginTop: theme.spacing(3)
   }
 }));
-
-async function urlToFile(url, filename, mimeType) {
-  const res = await fetch(url);
-  const buffer = await res.arrayBuffer();
-  return new File([buffer], filename, { type: mimeType });
-}
-
-function FileUploader(props) {
-  const classes = useStyles();
-  const [file, setFile] = useState(undefined);
-  const [fileName, setFileName] = useState("");
-  const [endpoint, setEndpoint] = useState(
-    "http://image-to-s3-babe-test.apps.spai.ml/_api/image"
-  );
-  const [isUploading, setIsUploading] = useState(false);
-  const [isShowWebcam, setIsShowWebcam] = useState(false);
-  const webcamRef = useRef(null);
-
-  /* <input type=file /> */
-  const fileEl = useRef(null);
-  const onBrowseClick = () => {
-    fileEl.current.click();
-  };
-
-  const onChangeFile = e => {
-    setFile(e.target.files[0]);
-    setFileName(e.target.files[0] ? e.target.files[0].name : "");
-  };
-
-  const onChangeFileName = e => {
-    setFileName(e.target.value);
-  };
-
-  const onChangeEndpoint = e => {
-    setEndpoint(e.target.value);
-  };
-
-  const onSubmit = async e => {
-    e.preventDefault();
-    setIsUploading(true);
-    try {
-      const res = await uploadFile(file, fileName, endpoint);
-      console.log(res.data);
-    } catch (error) {
-      console.log(error);
-    }
-    setIsUploading(false);
-  };
-
-  const uploadFile = async (file, fileName, endpoint) => {
-    const formData = new FormData();
-    formData.append("picture", file);
-    formData.append("pictureName", fileName);
-    return await fetch(endpoint, {
-      method: "POST",
-      body: formData
-    });
-  };
-
-  const onWebcamClick = async () => {
-    const imageSrc = webcamRef.current.getScreenshot();
-    const webcamFile = await urlToFile(
-      imageSrc,
-      `webcam-${Date.now()}.jpg`,
-      "image/jpeg"
-    );
-    setFile(webcamFile);
-    setFileName(webcamFile.name);
-    setIsShowWebcam(false);
-  };
-
-  const onWebcamToggle = () => {
-    setIsShowWebcam(!isShowWebcam);
-  };
-
-  return (
-    <form onSubmit={onSubmit}>
-      <TextField
-        type="text"
-        name="endpoint"
-        label="Endpoint"
-        value={endpoint}
-        onChange={onChangeEndpoint}
-        fullWidth
-        margin="normal"
-      />
-      <input
-        ref={fileEl}
-        type="file"
-        onChange={onChangeFile}
-        style={{ display: "none" }}
-      />
-      <TextField
-        disabled
-        type="text"
-        label="Selected Picture"
-        value={file ? file.name : ""}
-        margin="normal"
-        fullWidth
-        InputProps={{
-          endAdornment: (
-            <React.Fragment>
-              <InputAdornment position="end">
-                <IconButton>
-                  <CameraIcon color="secondary" onClick={onWebcamToggle} />
-                </IconButton>
-                <IconButton onClick={onBrowseClick}>
-                  <InsertDriveFileIcon color="secondary" />
-                </IconButton>
-              </InputAdornment>
-            </React.Fragment>
-          )
-        }}
-      />
-      {isShowWebcam && (
-        <Webcam
-          ref={webcamRef}
-          audio={false}
-          screenshotFormat="image/jpeg"
-          width="100%"
-          onClick={onWebcamClick}
-        />
-      )}
-      <TextField
-        type="text"
-        name="fileName"
-        label="Picture Name"
-        value={fileName}
-        onChange={onChangeFileName}
-        fullWidth
-        margin="normal"
-      />
-      {file && (
-        <img
-          src={URL.createObjectURL(file)}
-          alt={file.name}
-          style={{ width: "100%" }}
-        />
-      )}
-      <Button
-        fullWidth
-        variant="contained"
-        color="primary"
-        type="submit"
-        className={classes.button}
-        disabled={isUploading}
-      >
-        {isUploading ? <CircularProgress size={24} /> : "Upload"}
-      </Button>
-    </form>
-  );
-}
 
 function App() {
   const classes = useStyles();
@@ -193,9 +28,9 @@ function App() {
       <main className={classes.layout}>
         <Paper className={classes.paper}>
           <Typography component="h1" variant="h4" color="primary" gutterBottom>
-            Image Input
+            Image Upload Form
           </Typography>
-          <FileUploader />
+          <ImageUploadForm />
         </Paper>
       </main>
     </React.Fragment>
---
+++ src/components/imageUploadForm/index.js
@@ -0,0 +1,190 @@
+import React, { useState, useRef } from "react";
+import Webcam from "react-webcam";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Button,
+  CircularProgress,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Typography,
+  Box
+} from "@material-ui/core";
+import CameraIcon from "@material-ui/icons/Camera";
+import InsertDriveFileIcon from "@material-ui/icons/InsertDriveFile";
+
+const useStyles = makeStyles(theme => ({
+  inputFile: {
+    margin: `${theme.spacing(0.5)}px 0`
+  },
+  button: {
+    marginTop: theme.spacing(3)
+  }
+}));
+
+async function urlToFile(url, filename, mimeType) {
+  const res = await fetch(url);
+  const buffer = await res.arrayBuffer();
+  return new File([buffer], filename, { type: mimeType });
+}
+
+export default function ImageUploadForm(props) {
+  const [file, setFile] = useState(undefined);
+  const [fileName, setFileName] = useState("");
+  const [isUploading, setIsUploading] = useState(false);
+  const [endpoint, setEndpoint] = useState(
+    "https://image-to-s3-babe-test.apps.spai.ml/_api/image"
+  );
+
+  const uploadFile = async (file, fileName, endpoint) => {
+    if (isUploading) {
+      return;
+    }
+
+    setIsUploading(true);
+    const formData = new FormData();
+    formData.append("picture", file);
+    formData.append("pictureName", fileName);
+    try {
+      const res = await fetch(endpoint, {
+        method: "POST",
+        body: formData
+      });
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+    setIsUploading(false);
+  };
+
+  const [isShowWebcam, setIsShowWebcam] = useState(false);
+  const webcamRef = useRef(null);
+  const fileEl = useRef(null);
+
+  const onClickBrowseButton = () => {
+    fileEl.current.click();
+  };
+
+  const onChangeFileInput = e => {
+    setFile(e.target.files[0]);
+    setFileName(e.target.files[0] ? e.target.files[0].name : "");
+  };
+
+  const onChangeFileNameInput = e => {
+    setFileName(e.target.value);
+  };
+
+  const onChangeEndpointInput = e => {
+    setEndpoint(e.target.value);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    uploadFile(file, fileName, endpoint);
+  };
+
+  const onClickWebcam = async () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    const webcamFile = await urlToFile(
+      imageSrc,
+      `webcam-${Date.now()}.jpg`,
+      "image/jpeg"
+    );
+    setFile(webcamFile);
+    setFileName(webcamFile.name);
+    setIsShowWebcam(false);
+  };
+
+  const onClickWebcamToggle = () => {
+    setIsShowWebcam(!isShowWebcam);
+  };
+
+  const classes = useStyles();
+
+  return (
+    <form onSubmit={onSubmit}>
+      <TextField
+        type="text"
+        name="endpoint"
+        label="Endpoint"
+        value={endpoint}
+        onChange={onChangeEndpointInput}
+        fullWidth
+        margin="normal"
+      />
+      <input
+        ref={fileEl}
+        type="file"
+        onChange={onChangeFileInput}
+        style={{ display: "none" }}
+      />
+      <TextField
+        disabled
+        type="text"
+        label="Selected Image"
+        value={file ? file.name : ""}
+        margin="normal"
+        fullWidth
+        InputProps={{
+          endAdornment: (
+            <React.Fragment>
+              <InputAdornment position="end">
+                <IconButton onClick={onClickWebcamToggle}>
+                  <CameraIcon color="secondary" />
+                </IconButton>
+                <IconButton onClick={onClickBrowseButton}>
+                  <InsertDriveFileIcon color="secondary" />
+                </IconButton>
+              </InputAdornment>
+            </React.Fragment>
+          )
+        }}
+      />
+      {isShowWebcam && (
+        <Box marginTop={4}>
+          <Typography variant="body2" color="textSecondary">
+            Webcam
+          </Typography>
+          <Webcam
+            ref={webcamRef}
+            audio={false}
+            screenshotFormat="image/jpeg"
+            width="100%"
+            onClick={onClickWebcam}
+          />
+        </Box>
+      )}
+      <TextField
+        type="text"
+        name="fileName"
+        label="Image Name"
+        value={fileName}
+        onChange={onChangeFileNameInput}
+        fullWidth
+        margin="normal"
+      />
+      {file && (
+        <Box marginTop={2}>
+          <Typography variant="body2" color="textSecondary">
+            Preview Image
+          </Typography>
+          <img
+            src={URL.createObjectURL(file)}
+            alt={file.name}
+            style={{ width: "100%" }}
+          />
+        </Box>
+      )}
+      <Button
+        fullWidth
+        variant="contained"
+        color="primary"
+        type="submit"
+        className={classes.button}
+        disabled={isUploading}
+      >
+        {isUploading ? <CircularProgress size={24} /> : "Upload"}
+      </Button>
+    </form>
+  );
+}
